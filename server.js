e4b06ba43e8848262a492891bc27f954437206e6
krishansharma009 <godkrihna.apple.mtt@gmail.com>
2024-08-26 13:12:12 +0530

    moni

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ sequelize
   });
 
 app.use("/_alive", async (req, res) => {
-  res.status(200).send("Welcome to krishan");
+  res.status(200).send("Welcome to monika");
 });
 app.use("/users", userRouter);
 app.use("/candidate", candidateRouter);
